c358fad09a8265fce53d6ef1f45f61d9295c07fa
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-06-08 13:14:20 -0400

    mysql-clauses

---
+++ clauses.lec.sql
@@ -0,0 +1,32 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date = '1985-01-01';
+
+SELECT * FROM employees WHERE hire_date LIKE '1985%';
+
+SELECT DISTINCT hire_date, birth_date FROM employees WHERE hire_date LIKE '1985%' ORDER BY hire_date DESC, birth_date ASC;
+
+SELECT * FROM employees WHERE hire_date LIKE '%12-25';
+
+SELECT  * FROM employees WHERE hire_date LIKE '%-06-%';
+
+SELECT * FROM employees WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+
+SELECT * FROM employees WHERE last_name = 'Herber' AND hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE hire_date LIKE '%12-25' OR hire_date LIKE '%10-31';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no < 20000
+    AND (last_name IN ('Herber', 'Baek')
+    OR first_name = 'Shridhar');
+
+SELECT first_name, last_name FROM employees WHERE first_name = 'Munenori'
+LIMIT 9 OFFSET 17;
+
+USE codeup_test_db;
+
+INSERT INTO quotes (author_first_name, author_last_name, content) VALUES ('David', 'Stephens', '99 bottles of root beer on the wall...');
+
+SELECT * FROM quotes ORDER BY content;
